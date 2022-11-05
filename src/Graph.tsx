67d8efe22e983706ced0771301a1eddce770712c
Pratham Shroff <pshroff@bu.edu>
2022-11-05 11:18:55 -0400

    Updated element attributes

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -45,10 +45,15 @@ class Graph extends Component<IProps, {}> {
       this.table = window.perspective.worker().table(schema);
     }
     if (this.table) {
-      // Load the `table` in the `<perspective-viewer>` DOM reference.
-
-      // Add more Perspective configurations here.
-      elem.load(this.table);
+        // Load the `table` in the `<perspective-viewer>` DOM reference.
+        elem.load(this.table);
+        // Add more Perspective configurations here.
+        elem.setAttribute("view", "y_line");
+        elem.setAttribute("column-pivots", '["stock"]');
+        elem.setAttribute("row_pivots", '["timestamp"]');
+        elem.setAttribute("columns", '["top_ask_price"]');
+        elem.setAttribute("aggregates",
+            '{"stock":"distinct_count", "top_ask_price":"avg", "top_bid_price":"avg", "timestamp":"distinct_count"}');
     }
   }
 
